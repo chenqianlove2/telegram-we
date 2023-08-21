1c51f34e0b30d1815abae572143bc9c16e439c8b
chenqianlove2 <mc@cqjide.com>
2023-08-21 20:58:04 +0800

    Add files via upload

--- config.py
+++ config.py
@@ -2,8 +2,8 @@ master_channel = 'plugins.eh_telegram_master', 'TelegramChannel'
 slave_channels = [('plugins.eh_wechat_slave', 'WeChatChannel')]
 
 eh_telegram_master = {
-    "token": "xxxxxxxxxxxxxxxxx", # YOUR_TELEGRAM_BOT_TOKEN
-    "admins": [1234567890],       # YOUR_TELEGRAM_ID
+    "token": "6372101591:AAEjbF-12VFPNXniNGAxkWICO3jAPNqXcX8", # YOUR_TELEGRAM_BOT_TOKEN
+    "admins": [5903436778],       # YOUR_TELEGRAM_ID
     "bing_speech_api": ["xxx", "xxx"],
     "baidu_speech_api": {
         "app_id": 0,
